af42cb44c1289036194c92665175663dd588274f
smaniches <santiago.maniches@gmail.com>
2026-07-06 10:18:00 -0400

    chore(release): cut 0.1.0.dev3 (#48)

--- groupoid/__init__.py
+++ groupoid/__init__.py
@@ -1,6 +1,6 @@
 """GROUPOID: Groupoid-based federated learning with Riemannian geometry."""
 
-__version__ = "0.1.0.dev2"
+__version__ = "0.1.0.dev3"
 
 from groupoid.aggregation import (
     DisconnectedClientGraphError,
